5cdb5333da816ed72cd3036c42271d4a45b4d8dc
Jose Campos <desarrollador8@azsmart.net>
2025-04-23 09:14:17 -0500

    fix: se edita para utilizar la api de produccion

--- src/apis.jsx
+++ src/apis.jsx
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const baseUrl = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/';
+const baseUrl = import.meta.env.VITE_API_URL || 'https://vms-5saw.onrender.com/';
 const api = axios.create({ baseURL: baseUrl });
 export default api;
