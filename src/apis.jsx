7b37042f4d673832db336f619b7f8124585338e3
Jose Campos <desarrollador8@azsmart.net>
2025-04-24 08:49:35 -0500

    feat: actualizar la URL base para apuntar a producción

--- src/apis.jsx
+++ src/apis.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:8000';
+//const baseUrl = 'http://localhost:8000'; //Development URL
+const baseUrl = 'https://vms-5saw.onrender.com/'; //Production URL
 const api = axios.create({ baseURL: baseUrl });
 
 // Antes de cada petición, inyecta el Authorization si hay token
